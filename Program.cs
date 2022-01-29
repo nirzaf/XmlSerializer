a4c925af619c7a245af2be1d316bd498498bc53a
nirzaf <mfmfazrin1986@gmail.com>
2022-01-29 19:02:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using System.Xml.Serialization;
+
+
+Country country = new Country();
+country.CountryId = 1;
+country.CountryName = "Sri Lanka";
+
+XmlSerializer serializer = new XmlSerializer(typeof(Country));
+string filePath = @"E:\country.xml";
+
+FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+serializer.Serialize(fs, country);
+
+fs.Close();
+
+WriteLine("Xml file created");
+ReadKey();
+
+[Serializable]
+public class Country
+{
+    public int CountryId { get; set; }
+    public string CountryName { get; set; }
+}
+
+
+
+
